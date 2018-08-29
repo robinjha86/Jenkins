6248e5ce40af61acba957700442948a42767176b
robin.j <robin.j@WLPT908>
2018-08-29 13:33:54 +0530

    Add project files.

---
+++ JenkinsPOC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JenkinsPOC.MvcApplication" Language="C#" %>
---
+++ JenkinsPOC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JenkinsPOC.Startup))]
+namespace JenkinsPOC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
